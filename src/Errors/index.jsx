7ff0fbf96751674ffa8a6d5d73ffdf3ac39c1d2b
ptahemdjehuty <pariso03henri@gmail.com>
2023-02-24 13:16:30 +0100

    - init the repo

---
+++ src/Errors/index.jsx
@@ -0,0 +1,9 @@
+function _404(){
+    return(
+        <div>
+            <h1>Erreur ! Page Non trouvee</h1>
+        </div>
+    )
+}
+
+export default _404
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="App">
+      welcome
+    </div>
+  );
+}
+
+export default Home;
