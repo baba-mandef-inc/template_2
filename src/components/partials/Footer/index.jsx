f5046997f991dede21dcce8d134676facf70b16e
ptahemdjehuty <pariso03henri@gmail.com>
2023-04-21 04:49:15 +0100

    - add go to top button to footer

--- src/components/partials/Footer/index.jsx
+++ src/components/partials/Footer/index.jsx
@@ -109,6 +109,11 @@ function Footer(){
                 </div>
             </div>
         </div>
+        <div class="progress-wrap active-progress">
+            <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+                <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style={{transition: "stroke-dashoffset 10ms linear 0s", strokeDasharray: '307.919px, 307.919px', strokeDashoffset: '162.499px'}}></path>
+            </svg>
+        </div>
         </div>
     );
 
