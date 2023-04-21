43e05556c42c7ceeffe6e39659635cb476232de4
ptahemdjehuty <pariso03henri@gmail.com>
2023-04-21 04:49:47 +0100

    - Add projects

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,98 +1,110 @@
-function ProjectCard(){
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-    return (
-       /*  <!-- Case Study Start --> */
-        <div className={"section case-study-section section-padding"}>
-            <div  className={'container'}>
-                <div className={"case-study-wrap"}>
-                    <div className={"section-title text-center"}>
-                        <h3 className={"sub-title"}>Our Projects</h3>
-                        <h2 className={"title"}>Innovation a Passion</h2>
-                    </div>
-                </div>
-            </div>
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
 
-       {/*      <!-- Case Study Slider Wrap Start --> */}
-            <div className={"case-study-slider-wrap"}>
-                <div className={"swiper-container case-study-active"}>
-                    <div className={"swiper-wrapper"}>
-                        <div className={"swiper-slide"}>
-                            {/* <!-- Single Case Study Start --> */}
-                            <div className={"single-case-study text-center"} style={{backgroundImage: 'url(assets/images/c-study-1.jpg)'}}>
-                                <div className={"case-study-content"}>
-                                    <div className={"case-study-title"}>
-                                        <h3 className={"title"}>Aeroland Protocol</h3>
-                                        <span>Cyber Secutiy</span>
-                                    </div>
-                                    <div className={"case-study-text"}>
-                                        <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance talent for all your software development needs.</p>
-                                    </div>
-                                    <div className={"case-study-link"}>
-                                        <a href="#a"><i class="fas fa-long-arrow-alt-right"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                           {/*  <!-- Single Case Study End --> */}
-                        </div>
-                        <div className={"swiper-slide"}>
-                          {/*   <!-- Single Case Study Start --> */}
-                            <div class={"single-case-study text-center"} style={{backgroundImage: 'url(assets/images/c-study-2.jpg)'}}>
-                                <div className={"case-study-content"}>
-                                    <div className={"case-study-title"}>
-                                        <h3 className={"title"}>Analytic Solutions</h3>
-                                        <span>IT Technology</span>
-                                    </div>
-                                    <div className={"case-study-text"}>
-                                        <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance talent for all your software development needs.</p>
-                                    </div>
-                                    <div className={"case-study-link"}>
-                                        <a href="#a"><i className={"fas fa-long-arrow-alt-right"}></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <!-- Single Case Study End --> */}
-                        </div>
-                        <div className={"swiper-slide"}>
-                            {/* <!-- Single Case Study Start --> */}
-                            <div className={"single-case-study text-center"} style={{backgroundImage: 'url(assets/images/c-study-3.jpg)'}}>
-                                <div className={"case-study-content"}>
-                                    <div className={"case-study-title"}>
-                                        <h3 className={"title"}>Product Design</h3>
-                                        <span>IT Technology</span>
-                                    </div>
-                                    <div className={"case-study-text"}>
-                                        <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance talent for all your software development needs.</p>
-                                    </div>
-                                    <div className="{case-study-link}">
-                                        <a href="#a"><i className={"fas fa-long-arrow-alt-right"}></i></a>
-                                    </div>
+import './styles.css'
 
-                                </div>
-                            </div>
-                            {/* <!-- Single Case Study End --> */}
-                        </div>
-                    </div>
+// import required modules
+import { Navigation } from 'swiper'
 
-                    {/* <!-- Add Pagination --> */}
-                    <div className={"case-study-arrow swiper-button-next"}></div>
-                    <div className={"case-study-arrow swiper-button-prev"}></div>
-
-                    <div className={"swiper-pagination"}></div>
-                </div>
-            </div>
-           {/*  <!-- Case Study Slider Wrap End --> */}
-
-            {/* <!-- Case Study Button Start --> */}
-            <div className={"row jusify-content-center"}>
-                <div className={"case-study-btn text-center"}>
-                    <a className={"btn"} href="#a">See More Projects</a>
-                </div>
-            </div>
-           {/*  <!-- Case Study Button End --> */}
-
+function ProjectCard() {
+  return (
+    /*  <!-- Case Study Start --> */
+    <div className={'section case-study-section section-padding'} id="projects">
+      <div className={'container'}>
+        <div className={'case-study-wrap'}>
+          <div className={'section-title text-center'}>
+            <h3 className={'sub-title'}>Our Projects</h3>
+            <h2 className={'title'}>Innovation a Passion</h2>
+          </div>
         </div>
-    )
+      </div>
 
+      <div className="case-study-slider-wrap">
+        <div className="swiper-container case-study-active">
+          <Swiper
+            navigation={true}
+            modules={[Navigation]}
+            className="mySwiper swiper-wrapper"
+          >
+            <SwiperSlide>
+              <div
+                className={'single-case-study text-center'}
+                style={{ backgroundImage: 'url(assets/images/lp.jpg)' }}
+              >
+                <div className={'case-study-content'}>
+                  <div className={'case-study-title'}>
+                    <h3 className={'title'}>Let's Play</h3>
+                    <span> E-Sport social an beting App</span>
+                  </div>
+                  <div className={'case-study-text'}>
+                    <p>
+                      Let's Play is an amazing app for esport an gaming fans.
+                      This app helps you to interact with your favorites gamers
+                      and your gaming friends around the world. Chatting, News
+                      Feed, Virtual Esport Stadium, and Beting on your favorites
+                      gamers team match. What are you waiting for to join the
+                      ship ?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div
+                class={'single-case-study text-center'}
+                style={{ backgroundImage: 'url(assets/images/ea.png)' }}
+              >
+                <div className={'case-study-content'}>
+                  <div className={'case-study-title'}>
+                    <h3 className={'title'}>Ely Art</h3>
+                    <span>Movies Streaming App</span>
+                  </div>
+                  <div className={'case-study-text'}>
+                    <p>
+                      Ely art it's a mobile movies streaming application. This
+                      app provide you with a huge collection of african content
+                      producer and realisator movies and shows. What are you
+                      waiting for to chill in front on an amazing movie ?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={'swiper-slide'}>
+                {/* <!-- Single Case Study Start --> */}
+                <div
+                  className={'single-case-study text-center'}
+                  style={{ backgroundImage: 'url(assets/images/sam.png)' }}
+                >
+                  <div className={'case-study-content'}>
+                    <div className={'case-study-title'}>
+                      <h3 className={'title'}>SousAnonyMoi</h3>
+                      <span>Youtube channel content Production</span>
+                    </div>
+                    <div className={'case-study-text'}>
+                      <p>
+                        SousAnonyMoi is a youtube channel whose content is
+                        entirely produced by our team. What are you waiting for
+                        to join this youtube channel that offers you anonymous
+                        real life situations that are worthy of a movie?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* <!-- Single Case Study End --> */}
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ProjectCard
