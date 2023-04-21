c6b347f408daad1053d9737d253d74f3bf895b72
ptahemdjehuty <pariso03henri@gmail.com>
2023-04-21 05:24:45 +0100

    - Init team card

---
+++ src/components/TeamCard/index.jsx
@@ -0,0 +1,93 @@
+function TeamCard(){
+    return(
+        <>
+                 {/*    <!-- Team Start --> */}
+        <div class="section team-section section-padding">
+            <div class="container">
+               {/*  <!-- Team Wrap Start --> */}
+                <div class="team-wrap">
+                    <div class="section-title text-center">
+                        <h3 class="sub-title">Highly Professional Staffs </h3>
+                        <h2 class="title"> IT technology and technical fields professional staff</h2>
+                    </div>
+                   {/*  <!-- Team Content Wrap Start --> */}
+                    <div class="team-content-wrap">
+                        <div class="swiper-container team-active">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                   {/*  <!-- Single Team Start --> */}
+                                    <div class="single-team">
+                                        <div class="team-img">
+                                            <a href="team.html"><img src="assets/images/team/team-1.jpg" alt=""></img></a>
+                                        </div>
+                                        <div class="team-content">
+                                            <div class="team-social">
+                                                <ul class="social">
+                                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-behance"></i></a></li>
+                                                </ul>
+                                            </div>
+                                            <h3 class="name"><a href="#">Mike Holder</a></h3>
+                                            <span class="designation">Founder, Techmax</span>
+                                        </div>
+                                    </div>
+                                 {/*    <!-- Single Team End --> */}
+                                </div>
+                                <div class="swiper-slide">
+                                  {/*   <!-- Single Team Start --> */}
+                                    <div class="single-team">
+                                        <div class="team-img">
+                                            <a href="team.html"><img src="assets/images/team/team-2.jpg" alt=""></img></a>
+                                        </div>
+                                        <div class="team-content">
+                                            <div class="team-social">
+                                                <ul class="social">
+                                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-behance"></i></a></li>
+                                                </ul>
+                                            </div>
+                                            <h3 class="name"><a href="#">Mike Holder</a></h3>
+                                            <span class="designation">Founder, Techmax</span>
+                                        </div>
+                                    </div>
+                                   {/*  <!-- Single Team End --> */}
+                                </div>
+                                <div class="swiper-slide">
+                                  {/*   <!-- Single Team Start --> */}
+                                    <div class="single-team">
+                                        <div class="team-img">
+                                            <a href="team.html"><img src="assets/images/team/team-3.jpg" alt=""></img></a>
+                                        </div>
+                                        <div class="team-content">
+                                            <div class="team-social">
+                                                <ul class="social">
+                                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                                    <li><a href="#"><i class="fab fa-behance"></i></a></li>
+                                                </ul>
+                                            </div>
+                                            <h3 class="name"><a href="#">Mike Holder</a></h3>
+                                            <span class="designation">Founder, Techmax</span>
+                                        </div>
+                                    </div>
+                                    {/* <!-- Single Team End --> */}
+                                </div>
+                            </div>
+
+                          {/*   <!-- Add Pagination --> */}
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </div>
+                  {/*   <!-- Team Content Wrap End --> */}
+                </div>
+               {/*  <!-- Team Wrap End --> */}
+            </div>
+        </div>
+        </>
+    )
+}
